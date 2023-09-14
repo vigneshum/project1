b620677761d04d231c90d466b316beb64d8e57f1
vigneshum <145024506+vigneshum@users.noreply.github.com>
2023-09-14 06:17:42 -0700

    Add files via upload

---
+++ MarkApp1.py
@@ -0,0 +1,11 @@
+Name=" vignesh"
+plac="salem"
+Tamil=89
+English=90
+mas=78
+phy=88
+che=99
+computer=88
+Totel=Tamil+English+mas+phy+che+computer
+print("Thish is",Name,plac,"my Tamil mark",Tamil,"my English mark",English,"my mas mark",mas,"my phy mark",phy,"my che mark",che,"my computer mark",computer,"my Totel mark",Totel)
+
